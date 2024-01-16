3c6f7f529f3cea5627b9b4c15df087c6b1a2bedc
Roger Esteve <rogeresteve30@gmail.com>
2024-01-16 11:16:06 +0100

    leetify code had 3 lines in 1

--- leetify.js
+++ leetify.js
@@ -1,6 +1,8 @@
 const leetifyEloToRank = (ratingContainer) => {
     return new Promise((resolve, reject) => {
-        const lg = ratingContainer.getElementsByClassName("label-large ng-star-inserted")[0].innerHTML; const sm = ratingContainer.getElementsByClassName("label-small ng-star-inserted")[0].innerHTML; const elo =  parseInt(lg.substring(0, lg.length-1) + sm);
+        const lg = ratingContainer.getElementsByClassName("label-large ng-star-inserted")[0].innerHTML;
+        const sm = ratingContainer.getElementsByClassName("label-small ng-star-inserted")[0].innerHTML;
+        const elo = parseInt(lg.substring(0, lg.length - 1) + sm);
         fetch("https://whereisglobal.vercel.app/api/rank?elo=" + elo).then((data) => data.json()).then((data) => {
             resolve(data.rank);
         })
